d7e0af16058f1974a3161427faf638f7793bf8e6
Andreas <34609534+SchmidtAndreas1988@users.noreply.github.com>
2023-12-01 20:47:39 +0100

    a

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,6 @@
-    alert("hello");
-    var navbar = ` 
-        <nav>
-            <a href="../index.html">Homepage</a>
-        </nav>`;
+alert("hello");
 
-        // inserting navbar in beginning of body
-    document.head.insertAdjacentHTML("afterbegin", navbar);
+var navbar = '<nav><a href="../index.html">Homepage</a></nav>';
+
+// inserting navbar in beginning of body
+document.head.insertAdjacentHTML("afterbegin", navbar);
